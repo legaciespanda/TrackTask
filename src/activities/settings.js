eb7c391bf8c883b2d0d4d97b7da676a05e209397
Paul Oloyede <Paul Oloyede>
2021-01-21 00:39:31 +0100

    setting first draft

--- src/activities/settings.js
+++ src/activities/settings.js
@@ -1,22 +1,48 @@
-import React, { useEffect,Component,useState } from "react";
+import React, { useEffect, Component, useState } from "react";
 import {
   SafeAreaView,
+  Alert,
   StyleSheet,
   View,
   Image,
-  TouchableOpacity,
+  Platform,
+  Linking,
+  Share,
 } from "react-native";
 import { AppStyles } from "../config/styles";
 
-import { Container, Header, Content, Footer, FooterTab, 
+import {
+  Container, Header, Content, Footer, FooterTab,
   Icon, Text, Badge, Fab, Body, Title, Subtitle, Button,
-   ListItem, Left, Right, Switch   } from 'native-base';
+  ListItem, Left, Right, Switch
+} from 'native-base';
+import Communications from "react-native-communications";
+// import { useNavigation } from '@react-navigation/native';
+// import VersionCheck from 'react-native-version-check';
 
-import { useNavigation } from '@react-navigation/native';
 
 const SettingsActivity = ({ navigation }) => {
 
   const [active, setActive] = useState(true);
+
+
+  const shareWithFriends = () => {
+    const inputValue =
+      "Hello friends! Download Track Trace and install to access to the latest features";
+    const titleVal = "TrackTrace";
+    const urlVal = ""
+
+    //Here is the Share API
+    Share.share({
+      title: titleVal.toString(),
+      message: inputValue.toString(),
+      url:urlVal,
+    })
+      //after successful share return result
+      .then((result) => console.log(result))
+      //If any thing goes wrong it comes here
+      .catch((errorMsg) => console.log(errorMsg));
+  };
 
 
 
@@ -25,107 +51,190 @@ const SettingsActivity = ({ navigation }) => {
       {/* Banner */}
       <View
         style={{
-          flex: 1,}}
+          flex: 1,
+        }}
       >
- 
-
-            <Container>
-                <Header>
-                   <Body>
-                    <Title>Settings - TrackTask</Title>
-                   </Body>
-                 </Header>
 
 
-                <Content>
+        <Container>
+          <Header>
+            <Body>
+              <Title>Settings - TrackTask</Title>
+            </Body>
+          </Header>
 
 
-                <ListItem icon style={{ marginTop: 20 }}> 
-                  <Left>
-                    <Button style={{ backgroundColor: AppStyles.color.main}}>
-                      <Icon active name="newspaper" />
-                    </Button>
-                  </Left>
-                  <Body>
-                    <Text>About</Text>
-                  </Body>
-                </ListItem>
+          <Content>
 
-                <ListItem icon style={{ marginTop: 20 }}>
-                  <Left>
-                    <Button style={{ backgroundColor: AppStyles.color.main }}>
-                      <Icon active name="star" />
-                    </Button>
-                  </Left>
-                  <Body>
-                    <Text>Rate</Text>
-                  </Body>
+            <ListItem 
+            onPress={() =>
+              Alert.alert(
+                "About TrackTrace",
+                "A NubiaVille Limited App, built to increase work productivity and a performance appraisal tool \n\n Current Version: Beta-1.0",
 
-                </ListItem>
+                [
+                  {
+                    text: "Ok",
+                    onPress: () => {
+                      return null;
+                    },
+                  },
+                ],
+                { cancelable: false }
+              )
+            }
+            icon style={ styles.listItem }>
+              <Left>
+                <Button
+                 
+                 style={{ backgroundColor: AppStyles.color.main }}>
+                  <Icon active name="newspaper" />
+                </Button>
+              </Left>
+              <Body>
+                <Text>About</Text>
+                <Subtitle style={styles.subtitle}>Learn more about the TrackTrace App </Subtitle>
+              </Body>
+            </ListItem>
 
-                <ListItem icon style={{ marginTop: 20 }}>
-                  <Left>
-                    <Button style={{ backgroundColor: AppStyles.color.main }}>
-                      <Icon active name="person" />
-                    </Button>
-                  </Left>
-                  <Body>
-                    <Text>Invite Friends</Text>
-                  </Body>
-                </ListItem>
+            <ListItem
+             onPress={() => {
+              if (Platform.OS === "android") {
+                Linking.openURL(
+                  "https://play.google.com/store/apps/details?id=nic.goi.aarogyasetu&hl=en"
+                );
+              } else if (Platform.OS === "ios") {
+                Linking.openURL(
+                  "https://play.google.com/store/apps/details?id=nic.goi.aarogyasetu&hl=en"
+                );
+              }
+            }}
 
-                <ListItem icon style={{ marginTop: 20 }}> 
-                  <Left>
-                    <Button style={{ backgroundColor: AppStyles.color.main }}>
-                      <Icon active name="mail" />
-                    </Button>
-                  </Left>
-                  <Body>
-                    <Text>Contact</Text>
-                  </Body>
-                </ListItem>
+             icon style={ styles.listItem }>
+              <Left>
+                <Button style={{ backgroundColor: AppStyles.color.main }}>
+                  <Icon active name="star" />
+                </Button>
+              </Left>
+              <Body>
+                <Text>Rate</Text>
+                <Subtitle style={styles.subtitle}>To help us improve, Please Rate our app </Subtitle>
 
-                <ListItem icon style={{ marginTop: 20 }}>
-                  <Left>
-                    <Button style={{ backgroundColor: AppStyles.color.main }}>
-                      <Icon active name="ios-arrow-down"/>
-                    </Button>
-                  </Left>
-                  <Body>
-                    <Text>Download Update</Text>
-                  </Body>
-                </ListItem>
+              </Body>
 
-                <ListItem icon style={{ marginTop: 20 }}>
-                  <Left>
-                    <Button style={{ backgroundColor: AppStyles.color.main }}>
-                      <Icon active name="book" />
-                    </Button>
-                  </Left>
-                  <Body>
-                    <Text>Privacy Policy</Text>
-                  </Body>
-                </ListItem>
+            </ListItem>
 
-                </Content>
+            <ListItem 
+              onPress={() => shareWithFriends()}
+            icon style={ styles.listItem }>
+              <Left>
+                <Button style={{ backgroundColor: AppStyles.color.main }}>
+                  <Icon active name="person" />
+                </Button>
+              </Left>
+              <Body>
+                <Text>Invite Friends</Text>
+                <Subtitle style={styles.subtitle}>Share the App with Friends and Family </Subtitle>
 
-                <Footer>
-                <FooterTab>
-                    <Button onPress={() => navigation.navigate("HomeActivity") } active vertical>
-                    <Icon name="ios-add-circle" />
-                    <Text>Active Tasks</Text>
-                    </Button>
-                    <Button onPress={() => navigation.navigate("CompletedTaskActivity") } vertical>
-                    <Icon name="ios-checkbox" />
-                    <Text>Completed</Text>
-                    </Button>
-                    <Button onPress={() => navigation.navigate("SettingsActivity") } vertical>
-                    <Icon name="ios-settings" />
-                    <Text>Setings</Text>
-                    </Button>
-                </FooterTab>
-                </Footer>
-            </Container>
+              </Body>
+            </ListItem>
+
+            <ListItem icon style={ styles.listItem }>
+              <Left>
+                <Button style={{ backgroundColor: AppStyles.color.main }}>
+                  <Icon active name="mail" />
+                </Button>
+              </Left>
+              <Body>
+                <Text  
+                 onPress={() =>
+                  Alert.alert(
+                    "W'll like to Hear From You",
+                    "Please choose your preffered method to get in contact with TrackTrace.",
+                    [
+                      {
+                        text: "Send Email",
+                        onPress: () => {
+                          //send email
+                          Communications.email(
+                            ["support@nubiaville.onmicrosoft.com", "tech@nubiaville.onmicrosoft.com"],
+                            null,
+                            null,
+                            "Contact Nubiavill Limited",
+                            "Please write to us and we will resond in a short while"
+                          );
+                        },
+                      },
+      
+                      {
+                        text: "Phone Call",
+                      
+                        onPress: () => {
+                          Communications.phonecall("+2348052067585", true);
+                        },
+                      },
+                      {
+                        text: 'Cancel',
+                        onPress: () => console.log('Cancel Pressed'),
+                        style: 'cancel'
+                      },
+                      { cancelable: true },
+                    ],
+                  )
+                }
+                >Contact</Text>
+                <Subtitle style={styles.subtitle}>contact us if any issues, complains, etc</Subtitle>
+
+              </Body>
+            </ListItem>
+
+            <ListItem icon style={ styles.listItem }>
+              <Left>
+                <Button style={{ backgroundColor: AppStyles.color.main }}>
+                  <Icon active name="ios-arrow-down" />
+                </Button>
+              </Left>
+              <Body>
+                <Text>Download Update</Text>
+                <Subtitle style={styles.subtitle}>Check for updates </Subtitle>
+
+              </Body>
+            </ListItem>
+
+            <ListItem icon style={ styles.listItem }>
+              <Left>
+                <Button style={{ backgroundColor: AppStyles.color.main }}>
+                  <Icon active name="book" />
+                </Button>
+              </Left>
+              <Body>
+                <Text>Privacy Policy</Text>
+                <Subtitle style={styles.subtitle}>Please read our Privacy Policy </Subtitle>
+
+              </Body>
+            </ListItem>
+
+         
+
+          </Content>
+
+          <Footer>
+            <FooterTab>
+              <Button onPress={() => navigation.navigate("HomeActivity")} active vertical>
+                <Icon name="ios-add-circle" />
+                <Text>Active Tasks</Text>
+              </Button>
+              <Button onPress={() => navigation.navigate("CompletedTaskActivity")} vertical>
+                <Icon name="ios-checkbox" />
+                <Text>Completed</Text>
+              </Button>
+              <Button onPress={() => navigation.navigate("SettingsActivity")} vertical>
+                <Icon name="ios-settings" />
+                <Text>Setings</Text>
+              </Button>
+            </FooterTab>
+          </Footer>
+        </Container>
 
       </View>
     </SafeAreaView>
@@ -136,6 +245,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: AppStyles.color.white,
+  },
+  subtitle: {
+    color: AppStyles.color.subtitle,
+  },
+  listItem: {
+    marginTop: 20,
   }
 });
 
